a0921e5e9e11f2b69ba34e142a6c7784222c9132
samkayes <samanthasubaan@gmail.com>
2021-11-10 22:12:55 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState(0);
+  useEffect(() => {
+    fetch('https://api.github.com/users')
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+      setUsers(response)
+    })
+  },[]);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Welcome! Here is a list of GitHub users:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          (users!==0) ?
+            users.map(user => {
+              return (
+                <div class="container">
+                  <div class="top">
+                    <h1>{user.login}</h1>
+                  </div>
+                  <div class="bottom">
+                    <img src={user.avatar_url}/> 
+                    <p>User No: {user.id}</p>
+                    <p><a href={user.html_url}>Account link</a></p>
+                  </div>
+                </div>
+              )
+            }) : null
+        }
       </header>
     </div>
   );
